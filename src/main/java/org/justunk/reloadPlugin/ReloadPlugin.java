033902d719c195fdc31bb27c78121f2442c7c077
JustUnK <om238794@gmail.com>
2024-09-06 08:31:19 -0400

    Register commands and events

--- src/main/java/org/justunk/reloadPlugin/ReloadPlugin.java
+++ src/main/java/org/justunk/reloadPlugin/ReloadPlugin.java
@@ -1,12 +1,22 @@
 package org.justunk.reloadPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.justunk.reloadPlugin.events.TabCompletions;
+import org.justunk.reloadPlugin.commands.*;
+
+import java.util.Objects;
 
 public final class ReloadPlugin extends JavaPlugin {
+
+
 
     @Override
     public void onEnable() {
 
+
+        Objects.requireNonNull(getCommand("reloadplugin")).setExecutor(new ReloadPlugin());
+
+        Objects.requireNonNull(getCommand("reloadplugin")).setTabCompleter(new TabCompletions());
         Log("Loaded successfully");
 
 
